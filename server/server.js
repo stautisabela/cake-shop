eb7e511c6c8b87b311043219f50070ed2172c126
stautisabela <stautisabela@gmail.com>
2025-06-03 13:06:56 +0200

    Connect to DB

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const connectDB = require('./config/db');
 
-const app = express();
 const PORT = process.env.PORT || 5000;
-
-app.use(cors())
+const app = express();
+connectDB();
+app.use(cors());
 
 app.get('/api', (req, res) => {
       res.json({ "objectss": ["objOne", "objTwo", "objThree"] })
-})
+});
 
 app.listen(PORT, () => {
       console.log('Server running on port '+PORT)
-})
+});
