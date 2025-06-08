4cc0a4284f47355e2e38f9a811bba7d5741da5f0
stautisabela <stautisabela@gmail.com>
2025-06-08 16:22:02 +0200

    Add basic styling to navbar

--- client/src/components/Nvbar.js
+++ client/src/components/Nvbar.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router';
+import './Nvbar.css';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
+            {/* Brand Name */}
             <Link to="/" className="brand">
                 the butter the better
             </Link>
-            <ul>
-                <Link to='/about'>About</Link>
-                <Link to='/menu'>Menu</Link>
-                <Link to='/gallery'>Gallery</Link>
-                <Link to='/contact'>Contact</Link>
-            </ul>
+
+            {/* Navigation links */}
+            <div className='nav-links'>
+                <Link to='/about' className='nav-link'>About</Link>
+                <Link to='/menu' className='nav-link'>Menu</Link>
+                <Link to='/gallery' className='nav-link'>Gallery</Link>
+                <Link to='/contact' className='nav-link'>Contact</Link>
+            </div>
+
         </nav>
     )
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style.css';
 import App from './App';
 import { BrowserRouter } from 'react-router';
 import reportWebVitals from './reportWebVitals';
